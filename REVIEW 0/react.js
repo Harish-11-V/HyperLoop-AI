059892e94223c1a9998bcfbdf685071ea4db3fb4
HARISH KUMAR V <harishkumar11v@gmail.com>
2026-08-28 18:41:27 +0530

    Update react.js

--- REVIEW 0/react.js
+++ REVIEW 0/react.js
@@ -1,1 +1,14 @@
+import React from 'react';
+
+// A basic functional component
+function HelloWorld() {
+  return (
+    <div className="container">
+      <h1>Hello, World!</h1>
+      <p>Welcome to your first React component.</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
 
